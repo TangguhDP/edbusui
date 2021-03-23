d25e4225a4b43795dc6204d6d7d61a6bee3fc214
Tangguh Pramono <tangguhmyname@gmail.com>
2021-03-23 21:35:08 +0700

    Deleting app.css

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Landing() {
   return (
-    <div className="grid grid-cols-2 gap-x-20 gap-y-10 mx-auto">
+    <div className="grid grid-cols-2 gap-x-5 gap-y-10 mx-auto">
       <div className="w-full h-28 rounded-md bg-green-800 ring-1 ring-gray-400 shadow-lg" />
       <div className="w-full h-28 rounded-md bg-green-800 ring-1 ring-gray-400 shadow-lg" />
       <div className="w-full h-28 rounded-md bg-green-800 ring-1 ring-gray-400 shadow-lg" />
