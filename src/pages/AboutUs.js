5f1521292a24a44c50c1cf17247a0cd7a7e41e4c
Tangguh Pramono <tangguhmyname@gmail.com>
2021-03-25 14:40:31 +0700

    About Us page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function AboutUs() {
-    return (
-        <div>
-            AboutUs
+  return (
+    <>
+      <section className="text-gray-600">
+        <div className="container mx-auto flex md:flex-row flex-col items-center">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-full mb-10 md:mb-0">
+            <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="https://dummyimage.com/720x600"
+            />
+          </div>
+          <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-semibold text-gray-900">
+              Tangguh Destio Pramono
+            </h1>
+            <p className="mb-8 leading-relaxed text-justify md:text-left">
+              Copper mug try-hard pitchfork pour-over freegan heirloom neutra
+              air plant cold-pressed tacos poke beard tote bag. Heirloom echo
+              park mlkshk tote bag selvage hot chicken authentic tumeric
+              truffaut hexagon try-hard chambray.
+            </p>
+          </div>
         </div>
-    )
+      </section>
+      <section className="text-gray-600 body-font relative">
+        <div className="container mx-auto flex sm:flex-nowrap flex-wrap justify-center">
+          <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
+            <iframe
+              width="100%"
+              height="100%"
+              className="absolute inset-0"
+              frameBorder={0}
+              title="map"
+              marginHeight={0}
+              marginWidth={0}
+              scrolling="no"
+              src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+              style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
+            />
+            <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
+              <div className="lg:w-1/2 px-6">
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  ADDRESS
+                </h2>
+                <p className="mt-1">
+                  Photo booth tattooed prism, portland taiyaki hoodie neutra
+                  typewriter
+                </p>
+              </div>
+              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
+                  EMAIL
+                </h2>
+                <a href="#" className="text-indigo-500 leading-relaxed">
+                  example@email.com
+                </a>
+                <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
+                  PHONE
+                </h2>
+                <p className="leading-relaxed">123-456-7890</p>
+              </div>
+            </div>
+          </div>
+          <div className="flex self-center justify-center mt-4 w-full">
+            <Link
+              to="/register"
+              className="inline-flex w-full sm:hidden text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+            >
+              Join Grup WA
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
