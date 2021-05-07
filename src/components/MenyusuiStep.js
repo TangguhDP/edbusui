fa133e8bccfe420b29cecba6026e68573f061c51
Tangguh Pramono <tangguhmyname@gmail.com>
2021-05-07 10:08:35 +0700

    Adding Oksitosin material

--- src/components/MenyusuiStep.js
+++ src/components/MenyusuiStep.js
@@ -107,8 +107,8 @@ export default function MenyusuiStep() {
           </div>
         </div>
         <div className="flex relative pb-10 sm:items-center md:w-2/3 mx-auto">
-          <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
-            <div className="h-full w-1 bg-gray-200 pointer-events-none" />
+          <div className="h-full w-6 absolute inset-0 flex items-start justify-center">
+            <div className="h-1/6 md:h-1/3 w-1 bg-gray-200 pointer-events-none" />
           </div>
           <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-greenPea text-white relative z-10 title-font font-medium text-sm">
             6
