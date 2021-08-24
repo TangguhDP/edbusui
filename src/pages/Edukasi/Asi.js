fb055d7b3f92ef32d8662ece74a3725bcd0c0505
Tangguh Pramono <tangguhmyname@gmail.com>
2021-08-24 16:39:40 +0700

    video on edukasi

--- src/pages/Edukasi/Asi.js
+++ src/pages/Edukasi/Asi.js
@@ -18,6 +18,22 @@ export default function Asi() {
         "
         />
         <AsiStepper />
+        <section className="overflow-hidden text-gray-700 body-font md:mt-6">
+          <div className="container mx-auto lg:px-10">
+            <div className="flex flex-wrap md:flex-no-wrap">
+              <div className="w-full h-full">
+                <iframe
+                  title="video"
+                  className="object-cover object-center w-full md:w-5/6 mx-auto h-64 rounded-lg md:h-xxl"
+                  src="https://www.youtube.com/embed/YOUyl0nTuno"
+                  frameBorder="0"
+                  allow="autoplay; encrypted-media"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </section>
       </Wrapper>
     </>
   );
--- src/pages/Edukasi/Partner.js
+++ src/pages/Edukasi/Partner.js
@@ -17,6 +17,22 @@ export default function Partner() {
           "
         />
         <TextPartner />
+        <section className="overflow-hidden text-gray-700 body-font md:mt-6">
+          <div className="container mx-auto lg:px-10">
+            <div className="flex flex-wrap md:flex-no-wrap">
+              <div className="w-full h-full">
+                <iframe
+                  title="video"
+                  className="object-cover object-center w-full md:w-5/6 mx-auto h-64 rounded-lg md:h-xxl"
+                  src="https://www.youtube.com/embed/cHCWBQ4s3zY"
+                  frameBorder="0"
+                  allow="autoplay; encrypted-media"
+                  allowFullScreen
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </section>
       </Wrapper>
     </>
   );
